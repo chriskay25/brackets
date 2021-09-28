ff0ab00f95dab4fba39864f031ed54801ec329c0
Chris Kay <49329134+chriskay25@users.noreply.github.com>
2021-09-27 23:58:10 -0400

    Add containers for brackets and styling

--- src/App.js
+++ src/App.js
@@ -3,7 +3,11 @@ import './App.css';
 function App() {
   return (
     <div className="app">
-
+      <div className='brackets-container'>
+        <div className='brackets-container-inner'>
+          <h1>Brackets</h1>
+        </div>
+      </div>
     </div>
   );
 }
